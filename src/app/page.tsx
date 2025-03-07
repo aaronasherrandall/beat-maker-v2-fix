347a92527a78565eba23ee8c848132f04f132fc1
Aaron Randall <aaronasherrandall@gmail.com>
2025-03-07 13:01:40 -0500

    removed button import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,31 +6,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-zinc-900 text-white">
       <h1 className="text-3xl font-bold mb-8">MIDI Input Detector</h1>
       <MidiDetector />
-
-      {/* <div className='flex flex-row'>
-      {[...Array(4)].map((_, index) => (
-        <Button key={index}
-        label={`Button ${index + 1}`}
-        number={`${index + 1}`}
-        color={`${(index - 1) + 1}`}
-        disabled/>
-      ))}
-      </div>
-      <div className='flex flex-row'>
-      {[...Array(4)].map((_, index) => (
-        <Button key={index} label={`Button ${index + 1}`} number={`${index + 1}`} disabled/>
-      ))}
-      </div>
-      <div className='flex flex-row'>
-      {[...Array(4)].map((_, index) => (
-        <Button key={index} label={`Button ${index + 1}`} number={`${index + 1}`} disabled/>
-      ))}
-      </div>
-      <div className='flex flex-row'>
-      {[...Array(4)].map((_, index) => (
-        <Button key={index} label={`Button ${index + 1}`} number={`${index + 1}`} disabled/>
-      ))}
-      </div> */}
     </main>
   )
 }
