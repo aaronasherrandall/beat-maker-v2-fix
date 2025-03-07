98904d30dae64737b38bcb4d611ad4c13ad7b54a
Aaron Randall <aaronasherrandall@gmail.com>
2025-03-07 12:57:02 -0500

    deleted Button import in app -> page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import MidiDetector from "@/components/midi-detector"
-import Button from '../components/button'; // Correct import path for Button component
 
 export default function Home() {
   return (
